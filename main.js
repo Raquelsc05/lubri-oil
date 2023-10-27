f179f0ec1d7efe9cc357706988d4b2dfc086cf5a
Raquel <raquelcosta5@gmail.com>
2023-10-26 21:41:40 -0300

    whatsapp buttom

--- main.js
+++ main.js
@@ -1,59 +1,57 @@
 // Menu Responsivo //
- /* abre e fecha o menu quando clicar no icone: hamburguer e x */
- const nav = document.querySelector('#header nav')
- const toggle = document.querySelectorAll('nav .toggle')
- 
- for (const element of toggle) {
-   element.addEventListener('click', function () {
-     nav.classList.toggle('show')
-   })
- }
- 
- /* quando clicar em um item do menu, esconder o menu */
- const links = document.querySelectorAll('nav ul li a')
- 
- for (const link of links) {
-   link.addEventListener('click', function () {
-     nav.classList.remove('show')
-   })
- }
- 
- /* mudar o header da página quando der scroll */
- 
- const header = document.querySelector('#header')
- const navHeight = header.offsetHeight
- 
- if (window.scrollY >= navHeight) {
-   // scroll é maior que a altura do header
-   header.classList.add('scroll')
- } else {
-   // menor que a altura do header
-   header.classList.remove('scroll')
- }
- 
+/* abre e fecha o menu quando clicar no icone: hamburguer e x */
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
 
- 
- /* ScrollReveal */
- 
- const scrollReveal = ScrollReveal({
-   origin: 'top',
-   distance: '30px',
-   duration: 700,
-   reset: true
- })
- 
- scrollReveal.reveal(
-   `#home .image, #home .text,
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/* quando clicar em um item do menu, esconder o menu */
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/* mudar o header da página quando der scroll */
+
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+if (window.scrollY >= navHeight) {
+  // scroll é maior que a altura do header
+  header.classList.add('scroll')
+} else {
+  // menor que a altura do header
+  header.classList.remove('scroll')
+}
+
+/* ScrollReveal */
+
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
    #about .image, #about .text,
    #services header, #services .card,
    #brands header, #brands .brands
    #contact .text, #contact .links,
    footer .brand, footer .social
    `,
- 
-   { interval: 100 }
- )
- 
- window.addEventListener('scroll', function () {
-   changeHeaderWhenScroll()
- })
+
+  { interval: 100 }
+)
+
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+})
